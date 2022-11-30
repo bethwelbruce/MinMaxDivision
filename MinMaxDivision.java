98981c13ca9a7cab94da7128826b06e36086d117
BETHWEL NELSON OTIENO <bethwelbruce@gmail.com>
2022-11-30 10:07:31 +0300

    Add files via upload

---
+++ MinMaxDivision.java
@@ -0,0 +1,45 @@
+package CodelityTest;
+
+public class MinMaxDivision {
+	public	int	solution(int K,int M,int[]A) {
+		int min =0;
+		int max=0;
+		for(int a:A){
+			max +=a;
+			min=Math.max(min,max);
+			
+		}
+		
+		int bestAnswer=max;
+		while(min <=max) {
+		int mid =(min +max)/2;  
+		int blocks=checkBlocks(A,mid);
+		if(blocks>K) {
+			min=mid +1;
+		}else {
+			max=mid-1;
+			if(mid<bestAnswer) {
+				bestAnswer=mid;
+				}
+			}
+		}
+		return bestAnswer;		
+	}
+	private int checkBlocks(int[]A,int guess) {
+		int blocks =1;
+		int blockSum=0;
+		
+		for(int a:A) {
+			blockSum +=a;
+			if(blockSum >guess){
+				blockSum=a;
+				blocks++;
+				
+				
+			}
+		}
+		return blocks;
+	}
+	
+
+}
